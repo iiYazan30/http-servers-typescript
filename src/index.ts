d2bd5f0879d83e4eb6ce7172e13b8366e7308c50
iiYazan30 <yazanahmadmallah@gmail.com>
2026-08-02 23:15:25 +0300

    Add response logging middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,41 @@
-import express from "express";
+
+import express, { NextFunction, Request, Response } from "express";
+
+
+
 
 const app = express();
 const PORT = 8080;
+app.use(middlewareLogResponses);
 
-app.use(express.static("."));
+app.get("/healthz", handlerReadiness);
+
+app.use("/app", express.static("./src/app"));
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+function handlerReadiness(_req: Request, res: Response): void {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK");
+}
+
+
+function middlewareLogResponses(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  res.on("finish", () => {
+    const statusCode = res.statusCode;
+
+    if (statusCode < 200 || statusCode >= 300) {
+      console.log(
+        `[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`
+      );
+    }
+  });
+
+  next();
+}
